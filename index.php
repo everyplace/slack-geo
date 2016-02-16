4d32fd5e306e2dab1ceaa30dc2fd18abc8426f32
Apartm.net Server <erin@everyplace.net>
2016-02-16 07:39:38 -0500

    Fixed .env support, deactivated and removed auth key

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
 
+function pre_print($item) {
+	echo "<pre>";
+	var_dump($item);
+	echo "</pre>";
+}
+
 function parseEnv($environmental_variables) {
   $envs = file($environmental_variables);
   $envsObj = new stdClass;
   foreach($envs as $env) {
     $item = explode("=",$env);
-    $envsObj->$item[0] = $item[1];
+    $envsObj->$item[0] = trim($item[1]);
   }
   return $envsObj;
 }
 
-// $auth = ($_SERVER['AUTH']) ? $_SERVER['AUTH'] : parseEnv('.env')->auth;
-$auth = "xoxp-19250642657-19248205763-19260270998-b847394f84";
-
+$auth = ($_SERVER['AUTH']) ? $_SERVER['AUTH'] : parseEnv('.env')->AUTH;
 $autoloader = require __DIR__ . '/vendor/autoload.php';
 
 require __DIR__ . '/vendor/wrapi/slack/src/slack.php';
-// print_r($autoloader->getPrefixesPsr4());
 
 $client = new wrapi\slack($auth);
 $test = $client->emoji->list();
